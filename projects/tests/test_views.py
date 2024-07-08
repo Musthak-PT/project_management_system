1617fa2eba91e414dc52a1c6de498a9c2d38a08b
Musthak <musthak@aventusinformatics.com>
2024-07-08 17:39:36 +0530

    all the project test cases are ready

--- projects/tests/test_views.py
+++ projects/tests/test_views.py
@@ -1,4 +1,3 @@
-# tests/test_views.py
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
@@ -11,7 +10,7 @@ class ProjectViewTest(TestCase):
 
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(username='testuser', password='testpass', role='manager')
+        self.user = User.objects.create_user(username='testuser', password='testpass', role='admin')
         self.project = Project.objects.create(name="Test Project")
         self.project_url = reverse('projects:listing-project')
 
@@ -29,4 +28,25 @@ class ProjectViewTest(TestCase):
         }
         response = self.client.post(reverse('projects:create-or-update-project'), new_project_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        print(">>>>>>Creation>>>>>>>",response.data)
+        print(">>>>>Creation>>>>>", response.data)
+        
+        # Extract the ID of the created project for updating and deleting
+        project_id = response.data['data']['id']
+        
+        # Test project update
+        updated_project_data = {
+            'id': project_id,
+            'name': 'Updated Project',
+            'description': 'This is an updated project'
+        }
+        update_url = reverse('projects:create-or-update-project')
+        update_response = self.client.post(update_url, updated_project_data, format='json')
+        self.assertEqual(update_response.status_code, status.HTTP_201_CREATED)
+        print(">>>>>Update>>>>>", update_response.data)
+
+        # Test project deletion
+        delete_url = reverse('projects:delete-project')
+        delete_data = {'id': project_id}
+        delete_response = self.client.delete(delete_url, delete_data, format='json')
+        self.assertEqual(delete_response.status_code, status.HTTP_200_OK)
+        print(">>>>>Deletion>>>>>", delete_response.data)
